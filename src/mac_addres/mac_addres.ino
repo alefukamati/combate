3d54a7a667e3a3fad687a0de5e7634a7d1e35fbe
Ale <alekeiti@hotmail.com>
2023-01-10 14:10:56 -0600

    Comentários adicionados

--- src/mac_addres/mac_addres.ino
+++ src/mac_addres/mac_addres.ino
@@ -1,5 +1,5 @@
-// Código para a obtenção do MAC ADDRESS da ESP32 que receberá 
-// o sinal do controle
+// Código para a obtenção do MAC ADDRESS da ESP32 do robô
+// que receberá o sinal do controle. 
 #include <WiFi.h>
 
 void setup(){
